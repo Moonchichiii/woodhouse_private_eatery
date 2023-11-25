8bf9c48d81a4be3d643ed93872b88a81e459c5bd
Moonchichiii <>
2023-11-25 18:29:50 +0000

    prepared for first deployment

--- backend/settings.py
+++ backend/settings.py
@@ -43,8 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",    
     "cloudinary",
     "django.contrib.staticfiles",    
-    "cloudinary_storage",
-    "corsheaders",
+    "cloudinary_storage",    
 
     'django_bootstrap5',
 
@@ -140,7 +139,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 

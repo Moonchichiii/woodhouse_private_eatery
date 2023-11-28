c7fd204672b34808f50f09d5b3e24ef8e8a75980
Moonchichiii <>
2023-11-28 18:41:48 +0000

    last html validator error fixed

--- frontend/static/js/script.js
+++ frontend/static/js/script.js
@@ -1,16 +1,16 @@
-    let map;
+let map;
+
 async function initMap() {
-
   const { Map } = await google.maps.importLibrary("maps");
-  
+
   map = new Map(document.getElementById("map"), {
-    center: { lat: 56.629253, lng: 15.372672 },
-    zoom: 12,
+    center: { lat: 56.629253, lng:15.372672},
+    zoom: 8,
+  });
+}
 
-     });
-    }    
+initMap();
 
- initMap();
  
 
 document.addEventListener("DOMContentLoaded", function() {    
